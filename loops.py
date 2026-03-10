1c8bd8b2b5124b1b27069cb031ae1ab05b4a09f1
gr5175777-del <gr5175777@gmail.com>
2026-03-10 17:38:35 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,12 @@
+Python 3.14.3 (tags/v3.14.3:323c59a, Feb  3 2026, 16:04:56) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> products = ['bread','butter','milk','suger','salt']
+>>> for item in products:
+...     print(item)
+... 
+...     
+bread
+butter
+milk
+suger
+salt
