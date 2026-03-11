da6a3c132b82e93c849e315597b143146bb77d56
gr5175777-del <gr5175777@gmail.com>
2026-03-11 09:38:58 +0530

    Add files via upload

---
+++ conditional statement.py
@@ -0,0 +1,9 @@
+score = int(input("enter the score:"))
+if score >= 90:
+    print("grade: A")
+elif score >= 80:
+    print("graed: B")
+elif score >=70:
+    print("grade:C")
+else:
+    print("grade: F")
