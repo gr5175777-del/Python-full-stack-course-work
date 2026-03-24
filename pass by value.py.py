cdeee398032afbc3e81c212b94762e65f6ddc6c2
gr5175777-del <gr5175777@gmail.com>
2026-03-24 17:54:28 +0530

    Add files via upload

---
+++ pass by value.py.py
@@ -0,0 +1,21 @@
+#int,float,str,list,tuple,set,dict
+
+'''def display(n):
+    n=n+10
+    print("inside:",n)
+
+n=10
+display(n)
+print("outside:",n)'''
+
+
+
+
+
+def display(n):
+    n=[1,2,3,4]
+    print("inside:",n)
+
+n=[5,6,7]
+display(n)
+print("outside:",n)
