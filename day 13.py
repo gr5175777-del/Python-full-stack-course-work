daefe6c33a024573cce80c3d1551c9fb7956c710
gr5175777-del <gr5175777@gmail.com>
2026-03-26 17:16:16 +0530

    Add files via upload

---
+++ day 13.py
@@ -0,0 +1,22 @@
+'''a = []
+for i in range (1,100):
+     if i%2==0:
+         a.append(i)
+print(a)'''
+
+
+'''j=[i for i in range(2,100) if i%2 == 0]
+print(j)'''
+
+
+'''s='python programing'
+
+vol='aeiouaAEIOU'''
+
+l=[7,3,2,5,1,5,6,4,1,2,8,5,6,7,3,8]
+
+
+rl ={i:l.count(i) for i in l}
+print(rl)
+
+
