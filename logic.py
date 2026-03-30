dcb6d3a1dde64d2cc50423e45d128ebebf3aa874
gr5175777-del <gr5175777@gmail.com>
2026-03-30 22:00:44 +0530

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,26 @@
+'''likes = 0
+
+comments = []
+
+def addlikes():
+    global likes
+    likes+=1
+    return likes
+
+def addcomments(com=None):
+    comments.append(com)
+    return comments'''
+
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+def div(a,b):
+    return a/b
+def mod(a,b):
+    return a%b
+
