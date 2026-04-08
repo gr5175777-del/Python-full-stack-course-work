fd6c0867b388dc16be676eddb80584a40392ae88
gr5175777-del <gr5175777@gmail.com>
2026-04-08 23:21:30 +0530

    Add files via upload

---
+++ oops.book.py
@@ -0,0 +1,28 @@
+'''class book:
+    def __init__(self,title,author,price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def display_info(self):
+        print(f"title: {self.title},author:{self.author},price:${self.price}")
+
+#object creation and methods
+book1 = book("clean code", "robert martin",450)
+book1.display_info()'''
+
+
+
+'''class employee:
+    def __init__(self,name,base_salary):
+        self.name = name
+        self.base_salary = base_salary
+    def calculate_annual_salary(self):
+        print(self.base_salary * 12)
+
+employee1 = employee("govind",50000)
+employee1.calculate_annual_salary()'''
+
+
+
+
